c3407ffe6176b3bedc3293e73479051a3c3d9e89
stray-code <abc1007020@gmail.com>
2024-04-02 21:36:18 +0900

    crxjs追加と設定

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import { crx, defineManifest } from '@crxjs/vite-plugin'
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: "文字数カウント",
+  version: "1.0.0",
+  action: { default_popup: "index.html" }
+})
+
+export default defineConfig({
+  plugins: [crx({ manifest })],
+})
