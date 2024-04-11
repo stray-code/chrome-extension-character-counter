2db473c858f5ba25669d75ab7c16e1534e2b0070
stray-code <abc1007020@gmail.com>
2024-04-11 20:30:22 +0900

    画像のディレクトリ移動

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
-import { defineConfig } from 'vite'
-import { crx, defineManifest } from '@crxjs/vite-plugin'
+import { defineConfig } from "vite";
+import { crx, defineManifest } from "@crxjs/vite-plugin";
 
 const manifest = defineManifest({
   manifest_version: 3,
   name: "文字数カウント",
-  description: "テキストボックスに入力された文字数をリアルタイムにカウントします。",
+  description:
+    "テキストボックスに入力された文字数をリアルタイムにカウントします。",
   version: "1.0.0",
   icons: {
-    16: 'src/assets/img/icon16.png',
-    48: 'src/assets/img/icon48.png',
-    128: 'src/assets/img/icon128.png'
+    16: "img/icon16.png",
+    48: "img/icon48.png",
+    128: "img/icon128.png",
   },
   action: {
-    default_icon: 'src/assets/img/icon16.png',
-    default_popup: 'src/popup/index.html'
-  }
-})
+    default_icon: "img/icon16.png",
+    default_popup: "src/popup/index.html",
+  },
+});
 
 export default defineConfig({
   plugins: [crx({ manifest })],
@@ -26,4 +27,4 @@ export default defineConfig({
       port: 5173,
     },
   },
-})
+});
